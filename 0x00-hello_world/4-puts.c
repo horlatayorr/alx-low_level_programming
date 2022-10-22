dd7d8af66434a6bd9ea3d15afc1dfe9f8b8e52b8
Olatayo Adewuyi <olatayoadewuyi@gmail.com>
2022-10-21 22:34:30 -0700

    task done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+/**
+ * MyStr - Placeholder for string
+ * a blank line
+ * Puts - writes string to standard output
+ * a blank line
+ * 
+ * Return
+ */
+
 int main() 
 {
-	char MyStr[]= "Programming is like building a multilingual puzzle";
-
+	char MyStr[]="\"Programming is like building a multilingual puzzle";
+	
 	puts(MyStr);
 
 	return 0;
