41c7be91d776ec13bda4785899b432051fda5465
Olatayo Adewuyi <olatayoadewuyi@gmail.com>
2022-10-24 13:19:43 -0700

    changes made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry into code, returns void
+ * 
+ * @Mystr: Char data type that stores string
+ * Return: Always zero(success)
+ */
 int main(void)
 {
 	char MyStr[] = "\"Programming is like building a multilingual puzzle";
