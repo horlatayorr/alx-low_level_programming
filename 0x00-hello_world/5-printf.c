5a46b4bf53e37e713f8a27b0198dd909e16fbaf2
Olatayo Adewuyi <olatayoadewuyi@gmail.com>
2022-10-24 12:28:29 -0700

    changes made

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main: Entry point of code, 
+ * main: Entry point of code, returns void
 (* a blank line
  * printf: TThis prints out a string
  * Return: Always Zero (success)
-(*/
+(*
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
