576780f728deaed761e75b135dc9a9152219f23a
Olatayo Adewuyi <olatayoadewuyi@gmail.com>
2022-10-24 12:01:11 -0700

    changes made

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main: Entry
+ *
+ * return: Always Zero (success)
+ */
+int main (void)
+{
+    printf("with proper grammar, but the outcome is a piece of art\n");
+    return (0);
+}
