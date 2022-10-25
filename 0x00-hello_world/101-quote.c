70ba85d2e78bc99ab8409be790a4c519ffb384f2
Olatayo Adewuyi <olatayoadewuyi@gmail.com>
2022-10-25 14:10:08 -0700

    changes made

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry into code, returns void
+ *
+ *
+ * Return: Always returns 1(success)
+ */
+int main()
+{
+	char My_str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stdout, "%s", My_str);
+	return (1);
+}
