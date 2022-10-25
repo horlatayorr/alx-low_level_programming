7457a06ec882089b64616bf4d4a7d0ab6a9f8331
Olatayo Adewuyi <olatayoadewuyi@gmail.com>
2022-10-25 14:25:50 -0700

    changes made

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char My_str[] = "and that piece of art is useful\"  - Dora Korpar, 2015-10-19";
+	char My_str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	fprintf(stdout, "%s", My_str);
 	return (1);
