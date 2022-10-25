6f5cd7136522fec0853a6e5503638fb4e000346e
Olatayo Adewuyi <olatayoadewuyi@gmail.com>
2022-10-25 15:03:23 -0700

    changes made

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 
 /**
- * write - Function, takes date type char int and unsigned int
- *@filedes - integer data type
- *@*buf - string
- *@nbyte - unsigned integer data type
  * main - Entry into code, returns a string or int
  *
  *
  * Return: Always returns 1(Failure)
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
+	char My_str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	fprintf(stdout, "%s", My_str);
 	return (1);
 }
