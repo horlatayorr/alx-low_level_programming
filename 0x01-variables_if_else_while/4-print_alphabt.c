d1dfa88959be249fefe535a6eec4083c506f9f07
Olatayo Adewuyi <olatayoadewuyi@gmail.com>
2022-10-28 07:51:28 -0700

    changes made

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point of code, returns void
+ *
+ * Return: Always Zero(Success)
+ */
+
+int main(void)
+{
+	char ch;
+	ch = 'a';
+	
+	while(ch <= 'z') 
+	{
+		if(ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+			ch++;	
+		}
+		
+	}
+	ch = '\n';
+
+	putchar(ch);
+	return (0);
+}
